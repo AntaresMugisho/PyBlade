aa9f170196725ee34b8b243ab430b4f99fba1ea9
AntaresMugisho <antaresmugisho@gmail.com>
2024-10-22 20:57:46 +0200

    Template inheritence function done

--- pyblade/engine/parser.py
+++ pyblade/engine/parser.py
@@ -12,9 +12,8 @@ class Parser:
         self.directives = {
             "for": self._parse_for,
             "if": self._parse_if,
-            "include": self._parse_include,
-            "yield": self._parse_yield,
             "extends": self._parse_extends,
+            "include": self._parse_include,
         }
 
     def parse(self, template: str, context: dict) -> str:
@@ -86,7 +85,6 @@ class Parser:
         Get the line number where a variable is located in the template.
         Useful for debug.
         """
-
         return match.string.count("\n", 0, match.start()) + 1
 
     def _parse_if(self, template, context):
@@ -148,21 +146,87 @@ class Parser:
         return "".join(result)
 
     def _parse_include(self, template, context):
+        """Find partials code to include in the template"""
+
         pattern = re.compile(r"@include\s*\(\s*[\"']?(.*?(?:\.?\.*?)*)[\"']?\s*\)", re.DOTALL)
         match = re.search(pattern, template)
 
-        file_name = match.group(1) if match else None
-        partial_template = load_template(file_name) if file_name else None
+        if match is not None:
+            file_name = match.group(1) if match else None
+            partial_template = load_template(file_name) if file_name else None
 
-        if partial_template:
-            # Parse the content to include before replacement
-            partial_template = self.parse(partial_template, context)
-            return re.sub(pattern, partial_template, template)
+            if partial_template:
+                # Parse the content to include before replacement
+                partial_template = self.parse(partial_template, context)
+                return re.sub(pattern, partial_template, template)
 
-        return template
-
-    def _parse_yield(self, template, context):
         return template
 
     def _parse_extends(self, template, context):
+        """Search for extends directive in the template then parse sections inside."""
+
+        pattern = re.compile(r"(.*?)@extends\s*\(\s*[\"']?(.*?(?:\.?\.*?)*)[\"']?\s*\)", re.DOTALL)
+        match = re.match(pattern, template)
+
+        if match is not None:
+            if match.group(1):
+                raise Exception("The @extend tag must be at the top of the file before any character.")
+
+            layout_name = match.group(2) if match else None
+            if not layout_name:
+                raise Exception("Layout not found")
+
+            try:
+                layout = load_template(f"layouts.{layout_name}")
+                return self._parse_section(template, layout)
+            except Exception as e:
+                raise e
+
         return template
+
+    def _parse_section(self, template, layout):
+        """
+        Find every section that can be yielded in the layout
+
+        :param template: The partial template content
+        :param layout: The layout content in which sections will be yielded
+        :return: The full page after yield
+        """
+
+        pattern = re.compile(r"@section\s*\((?P<section_name>[^)]*)\)\s*(?P<content>.*?)@endsection", re.DOTALL)
+        matches = pattern.findall(template)
+
+        local_context = {}
+        for match in matches:
+            argument, content = match
+            line_match_pattern = re.compile(rf"@section\s*\(({argument})\)", re.DOTALL)
+            section_name = self._validate_argument(line_match_pattern.search(template))
+
+            local_context[section_name] = content
+            # TODO: Add a slot variable that will contain all the content outside the @section directives
+
+        return self._parse_yield(layout, local_context)
+
+    def _parse_yield(self, layout, context):
+        """
+        Replace every yieldable content by the actual value or None
+
+        :param layout:
+        :param context:
+        :return:
+        """
+        pattern = re.compile(r"@yield\s*\(\s*(?P<yieldable_name>.*?)\s*\)", re.DOTALL)
+        return pattern.sub(lambda match: self._handle_yield(match, context), layout)
+
+    def _handle_yield(self, match, context):
+        yieldable_name = self._validate_argument(match)
+        return context.get(yieldable_name)
+
+    def _validate_argument(self, match):
+        argument = match.group(1)
+        if (argument[0], argument[-1]) not in (('"', '"'), ("'", "'")) or len(argument.split(" ")) > 1:
+            raise Exception(
+                f"{argument} is not a valid string. Argument must be of type string."
+                f"Look at line {self._get_line_number(match)}"
+            )
+        return argument[1:-1]
--- pyblade/engine/renderer.py
+++ pyblade/engine/renderer.py
@@ -1,5 +1,4 @@
-from pathlib import Path
-
+from .loader import load_template
 from .parser import Parser
 
 
@@ -10,36 +9,13 @@ class PyBlade:
 
     def render(self, template: str, context: dict) -> str:
         """
-        Render the parsed template content
+        Render the parsed template content with replaced values.
 
         :param template:
         :param context:
         :return:
         """
-        template = self._load_template(template)
+        template = load_template(template)
         template = self.parser.parse(template, context)
 
         return template
-
-    @staticmethod
-    def _load_template(template_name: str) -> str:
-        """
-        Loads the template file.
-
-        :param template_name: The template name.
-        :return: The template content as string.
-        """
-
-        BASE_DIR = Path(__file__).parent.parent.parent
-        TEMPLATES_DIR = BASE_DIR.joinpath("templates")
-
-        folders = [a for a in template_name.split(".")]
-        file_path = TEMPLATES_DIR
-        for folder in folders:
-            file_path = file_path.joinpath(folder)
-
-        try:
-            with open(f"{file_path}.html", "r") as file:
-                return file.read()
-        except FileNotFoundError as e:
-            raise e
